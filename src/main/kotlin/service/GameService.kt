321452a4ccd9a7bd7516292c083955b5d2b536dd
alexander.feld <alexander.feld@tu-dortmund.de>
2025-03-07 15:25:29 +0100

    add: added KDocs and moved CardImageLoader to gui

---
+++ src/main/kotlin/service/GameService.kt
@@ -0,0 +1,110 @@
+package service
+
+class GameService {
+
+    /**
+     * Starts a new game and prepares different game elements
+     * @param players A list of pairs that represent a player.
+     * The pair consists of the player name and the type of the player as number:
+     * local player (0), remote player (1), random bot (2), smart bot (3)
+     * @param speed The simulation speed of the game.
+     * @param remote If the game is played in remote or in hotseat mode.
+     *
+     * Preconditions:
+     * - The application is running.
+     * - The sorting order of the players has been defined.
+     * - The type of the players has been defined.
+     * - The simulation speed has been set.
+     * - The game type has been set.
+     *
+     * Postconditions:
+     * - A game was created.
+     * - The goal plates have been configured.
+     * - The card shop has been prepared.
+     * - The tile storage has been prepared.
+     * - Every player has a plant pot and a seishi plate.
+     * - The inventories of the players have been filled with tiles.
+     *
+     * @return This method does not return anything ('Unit').
+     *
+     * @throws IllegalArgumentException If a player name or type is missing.
+     * @throws IllegalArgumentException If the simulation speed is not set.
+     * @throws IllegalArgumentException If game type and player types are not matching
+     * (e.g. local players in remote).
+     *
+     * @sample startNewGame(mutableListOf(Pair("Max Mustermann",0)),3,false)
+     */
+    fun startNewGame(players: List<Pair<String, Int>>, speed: Int, remote: Boolean) {
+        //Method implementation
+    }
+
+    /**
+     * Ends the game and evaluates which player won.
+     *
+     * This method does not accept parameters.
+     *
+     * Preconditions:
+     * - A game was started and is running.
+     * - All cards have been revealed.
+     *
+     * Postconditions:
+     * - All players are evaluated based on their goal tiles, as well as on the basic multipliers
+     *   and parchment card multipliers applied to their bonsai and hidden cards.
+     * - The player with the highest number of points is declared the winner.
+     *
+     * @returns This method does not return anything (`Unit`).
+     *
+     * @throws IllegalStateException If there is no started and running game,
+     *                               or if at least one card has not been revealed.
+     *
+     * @sample endGame()
+     */
+    fun endGame() {
+        // Method implementation
+    }
+
+    /**
+     * Loads a game from save file
+     *
+     * This method loads and unserializes game state from a save file and
+     * notifies the UI layer to update the game sense.
+     *
+     * Preconditions:
+     * - The application is running.
+     * - The save file exists.
+     * - The save file is readable.
+     *
+     * Postconditions:
+     * - The game was created.
+     * - The goal plates have returned to the recorded state.
+     * - The card shop has returned to the recorded state.
+     * - The tile storage has returned to the recorded state.
+     * - All player's plan pots and cards return to the recorded state.
+     * - The undo/redo Stack has returned to the recorded state
+     *
+     * @throws IllegalStateException if the save file doesn't exist
+     * @throws IOException if the save file can't be read
+     */
+    fun loadGame() {}
+
+    /**
+     * Saves the current game state to a save file
+     *
+     * This method serializes the current game state and writes it to a save file.
+     * If the save file in the default path doesn't exist, it will be created, otherwise
+     * it will be overwritten.
+     *
+     * Preconditions:
+     * - The game was started and is running.
+     * - When the save file already exists, it is writable.
+     * - The save file path is writable.
+     *
+     * Postconditions:
+     * - If the save file didn't exist, it was created.
+     * - Otherwise, it was overwritten by the newest serialized game state.
+     *
+     * @throws IllegalStateException if the game hasn't started yet
+     * @throws IOException if the save file can't be written to
+     */
+    fun saveGame() {}
+}
---
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -0,0 +1,121 @@
+package service
+
+class PlayerActionService {
+    /**
+     * Ends the active players turn and advances the game to the next player.
+     *
+     * This method does not accept parameters.
+     *
+     * Preconditions:
+     * - A game was started and is running.
+     * - The active player must have started an action (either `meditate` or `cultivate`).
+     *
+     * Postconditions:
+     * - If the player has more tiles than his storage limit allows,
+     *   he has to choose tiles to discard, until he doesn't.
+     * - The game saves the current state to the `undoStack` and clears the `redoStack`.
+     * - If all cards are revealed, increase the `endGameCounter`.
+     * - If the `endGameCounter` is larger than the player count, call `endGame`,
+     *   otherwise advance to the next player.
+     *
+     * @returns This method does not return anything (`Unit`).
+     *
+     * @throws IllegalStateException If there is no started and running game,
+     *                               or if the active player has not started an action.
+     *
+     * @sample endTurn()
+     */
+    fun endTurn() {
+        // Method implementation
+    }
+
+    /**
+     * Undoes the last action by popping and restoring the last state from the [undoStack]
+     * and pushing it onto the [redoStack].
+     *
+     * This method does not accept parameters.
+     *
+     * **Preconditions:**
+     * - A game has been started and is currently running.
+     * - The [undoStack] is not empty.
+     *
+     * **Postconditions:**
+     * - The previous game state has been moved from the [undoStack] to the [redoStack].
+     * - The previous game state has been restored.
+     *
+     * @throws IllegalStateException If there is no active game, or no action to undo.
+     */
+    fun undo() {
+        // Method implementation
+    }
+
+    /**
+     * Redoes the last undone action by popping and restoring the last state from the [redoStack]
+     * and pushing it onto the [undoStack].
+     *
+     * **Preconditions:**
+     * - A game has been started and is currently running.
+     * - The [redoStack] is not empty.
+     *
+     * **Postconditions:**
+     * - The following game state has been moved from the [redoStack] to the [undoStack].
+     * - The following game state has been restored.
+     *
+     * @throws IllegalStateException If there is no active game, or no action to redo.
+     */
+    fun redo() {
+        // Method implementation
+    }
+
+    /**
+     * Executes the "cultivate" action, allowing the player to place tiles in Bonsai.
+     * In the rare case that placing a wood tile is not possible at the beginning of the player's turn,
+     * the player may remove existing tiles to enable placement.
+     *
+     * Preconditions:
+     * - A game must have been started and must be currently running.
+     * - The player must be on their turn.
+     *
+     * Postconditions:
+     * - If placing a tile is possible , and the player chooses to, it will be placed.
+     * - If the player chooses to remove tiles, they will be removed.
+     * - The player's turn will end.
+     *
+     * @returns This method returns no value (`Unit`).
+     *
+     * @throws IllegalStateException If there is no active game.
+     *
+     * @sample cultivate()
+     */
+    fun cultivate() {
+        // Method implementation
+    }
+
+    /**
+     * Executes the "meditate" action, allowing the player to remove tiles and draw a card from the [BonsaiGame] [openCards].
+     * Based on the card position, a [Player] receives bonsai tiles from the common supply and keep
+     * them in his [supply].
+     * Based on the card type, a player can play a tile, recieve tiles or get a bonus
+     *
+     * Preconditions:
+     * - A [BonsaiGame] must have been started and must be currently running.
+     * - [Player] must be on their turn.
+     * - the Board must have at least one card
+     *
+     * Postconditions:
+     * - [HelperCard],[MasterCard] and [ParchmentCard] will be added to [Player] [hiddenDeck].
+     * - [ToolCard] will be added to [Player] [seichiTool].
+     * - [GrowthCard] will be added to [Player] [seichiGrowth].
+     * - The player's turn will end.
+     *
+     * @returns This method returns no value (`Unit`).
+     *
+     * @throws IllegalStateException If there is no active game.
+     * @throws IllegalStateException If the Game's stacks are empty.
+     *
+     */
+    fun medidate() {
+        // Method implementation
+    }
+
+}
