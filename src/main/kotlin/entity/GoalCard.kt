a10dfa7a0c25027a19f2b08f885b355575d5d386
Silas Menge <silas.menge@tu-dortmund.de>
2025-03-10 18:51:19 +0100

    Add difficulty to goal cards

--- src/main/kotlin/entity/GoalCard.kt
+++ src/main/kotlin/entity/GoalCard.kt
@@ -5,8 +5,10 @@ package entity
  *
  * @property points The number of points the player gains, if the card is acquired.
  * @property color Indicates which goals are part of the same group.
+ * @property difficulty The difficulty of the goal.
  */
 class GoalCard(
     val points: Int,
-    val color: GoalColor
+    val color: GoalColor,
+    val difficulty: GoalDifficulty,
 )
