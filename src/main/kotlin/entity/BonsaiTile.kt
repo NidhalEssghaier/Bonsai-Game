913fabd986c4e36ba1ec2eb13c7ec779393c4d12
anas.gharbi <anas.gharbi@tu-dortmund.de>
2025-03-11 00:38:19 +0100

    bare minimum of start game to be able to test player actions

--- src/main/kotlin/entity/BonsaiTile.kt
+++ src/main/kotlin/entity/BonsaiTile.kt
@@ -6,5 +6,9 @@ package entity
  * @property type The type of the tile (e.g. wood)
  */
 class BonsaiTile(
-    val type: TileType
-)
+    val type: TileType,
+){
+    //used to track neighbors to help in remove tiles and place fruit and get flower score
+    val neighbors: MutableList<Bonsai> = mutableListOf()
+
+}
