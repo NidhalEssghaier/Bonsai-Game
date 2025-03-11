d60db029801c5acebb18469ba189528f8ce875d7
Ao Zhong <hacc1225@gmail.com>
2025-03-11 15:16:39 +0100

    feat: add copy function for BidirectionalMap

--- src/main/kotlin/GlobalHelper.kt
+++ src/main/kotlin/GlobalHelper.kt
@@ -1,7 +1,22 @@
+import tools.aqua.bgw.util.BidirectionalMap
+
 /**
  * Copy a list
  * @return a copy of the original list
  */
 fun <T> List<T>.copy(): List<T> {
     return this.map { it }
+}
+
+/**
+ * Copy a BidirectionalMap
+ * @return a copy of the original BidirectionalMap
+ */
+fun <T : Any, R: Any> BidirectionalMap<T, R>.copy(): BidirectionalMap<T, R> {
+    val copy = BidirectionalMap<T, R>()
+    val keys = this.keysForward
+    for (key in keys) {
+        copy.put(key, this.forward(key))
+    }
+    return copy
 }
