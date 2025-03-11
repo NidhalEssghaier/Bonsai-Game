c514ac79325b63f0828a58eb59e6bf1785227920
Ao Zhong <hacc1225@gmail.com>
2025-03-11 15:37:34 +0100

    feat: add copy function for HexagonGrid

--- src/main/kotlin/GlobalHelper.kt
+++ src/main/kotlin/GlobalHelper.kt
@@ -1,3 +1,5 @@
+import tools.aqua.bgw.components.container.HexagonGrid
+import tools.aqua.bgw.components.gamecomponentviews.HexagonView
 import tools.aqua.bgw.util.BidirectionalMap
 
 /**
@@ -17,6 +19,22 @@ fun <T : Any, R: Any> BidirectionalMap<T, R>.copy(): BidirectionalMap<T, R> {
     val keys = this.keysForward
     for (key in keys) {
         copy.put(key, this.forward(key))
+    }
+    return copy
+}
+
+/**
+ * Copy a HexagonGrid
+ * @return a copy of the original HexagonGrid
+ */
+fun <T : HexagonView> HexagonGrid<T>.copy(): HexagonGrid<T> {
+    val copy = HexagonGrid<T>()
+    val map = this.getCoordinateMap()
+    for (coordinate in map.keys) {
+        val value = map[coordinate]
+        if (value != null) {
+            copy[coordinate.first, coordinate.second] = value
+        }
     }
     return copy
 }
