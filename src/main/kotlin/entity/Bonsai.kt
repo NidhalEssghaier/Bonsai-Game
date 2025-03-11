1e87b650c0505198deddbfb71f152f997c0e449e
Ao Zhong <hacc1225@gmail.com>
2025-03-11 20:14:26 +0100

    feat: enhance copy method in Bonsai class to fix neighbor references

--- src/main/kotlin/entity/Bonsai.kt
+++ src/main/kotlin/entity/Bonsai.kt
@@ -14,16 +14,21 @@ import tools.aqua.bgw.util.BidirectionalMap
 class Bonsai(
     val grid: HexagonGrid<HexagonView> = HexagonGrid(coordinateSystem=HexagonGrid.CoordinateSystem.AXIAL),
     val map: BidirectionalMap<HexagonView, BonsaiTile> = BidirectionalMap(),
-    val tileCount: MutableMap<TileType,Int> = TileType.entries.associateWith { 0 }.toMutableMap(),
-    val tiles: MutableList<BonsaiTile> = mutableListOf()
+    val tileCount: MutableMap<TileType,Int> = TileType.entries.associateWith { 0 }.toMutableMap()
 )
 {
+    val tiles = {map.keysBackward.toList()}
+
     fun copy(): Bonsai {
-        return Bonsai(
-            grid.copy(),
-            map.copy(),
-            tileCount.toMutableMap(),
-            tiles.toMutableList()
-        )
+        val copy = Bonsai(grid.copy(), map.copy(), tileCount.toMutableMap())
+
+        // Fix neighbors references
+        val tilesListCopy = copy.tiles()
+        for (copiedTile in tilesListCopy) {
+            val newNeighbors = copiedTile.neighbors.map { old -> copy.map.forward(map.backward(old)) }
+            copiedTile.neighbors.clear()
+            copiedTile.neighbors.addAll(newNeighbors)
+        }
+        return copy
     }
 }
