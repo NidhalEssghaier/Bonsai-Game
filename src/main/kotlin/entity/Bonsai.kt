d8aaac2bac7fc5bbadb2b6d3ea3a145c9379c155
Ao Zhong <hacc1225@gmail.com>
2025-03-11 14:35:02 +0100

    refactor: Modify the Bonsai class to correct the data storage mechanism
    (cherry picked from commit 336b5db0caeeeb69a6c966137305deea4f383c84)

--- src/main/kotlin/entity/Bonsai.kt
+++ src/main/kotlin/entity/Bonsai.kt
@@ -2,6 +2,7 @@ package entity
 
 import tools.aqua.bgw.components.container.HexagonGrid
 import tools.aqua.bgw.components.gamecomponentviews.HexagonView
+import tools.aqua.bgw.util.BidirectionalMap
 
 /**
  * Entity to represent the bonsai of a player
@@ -10,7 +11,8 @@ import tools.aqua.bgw.components.gamecomponentviews.HexagonView
  * @property tiles Saves used tiles as list.
  */
 class Bonsai(
-    val grid: MutableMap<HexagonGrid<HexagonView>,BonsaiTile>,
+    val grid: HexagonGrid<HexagonView>,
+    val map: BidirectionalMap<HexagonView, BonsaiTile>,
     val tileCount: Map<TileType,Int>,
     val tiles: MutableList<BonsaiTile>
 )
