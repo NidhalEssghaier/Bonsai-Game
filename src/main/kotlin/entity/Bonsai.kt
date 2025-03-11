2f407935cdeff1634c28a58b479f4295117e290c
Ao Zhong <hacc1225@gmail.com>
2025-03-11 21:24:44 +0100

    fix: improve documentation for Bonsai class properties and the copy method

--- src/main/kotlin/entity/Bonsai.kt
+++ src/main/kotlin/entity/Bonsai.kt
@@ -9,8 +9,10 @@ import tools.aqua.bgw.util.BidirectionalMap
 /**
  * Entity to represent the bonsai of a player
  *
- * @property grid Saves the positions of tiles in the bonsai.
- * @property tiles Saves used tiles as list.
+ * @property grid Store the [HexagonView] and its coordinates in the bonsai.
+ * @property map [BidirectionalMap] to store the mapping between [HexagonView] and [BonsaiTile].
+ * @property tileCount Saves the count of each tile type.
+ * @property tiles Returns the list of all tiles in the bonsai.
  */
 class Bonsai(
     val grid: HexagonGrid<HexagonView> = HexagonGrid(coordinateSystem=HexagonGrid.CoordinateSystem.AXIAL),
@@ -20,6 +22,11 @@ class Bonsai(
 {
     val tiles = {map.keysBackward.toList()}
 
+    /**
+     * Method to deep copy the bonsai
+     *
+     * @return [Bonsai] deep copy of the bonsai
+     */
     fun copy(): Bonsai {
         val copy = Bonsai(grid.copy(), map.deepBonsaiTileCopy(), tileCount.toMutableMap())
 
