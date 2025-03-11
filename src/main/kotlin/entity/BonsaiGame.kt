f750105c3e8ee573a7a1fc0055b1919295eb87b4
Ao Zhong <hacc1225@gmail.com>
2025-03-11 21:12:45 +0100

    fix: add currentState property to BonsaiGame documentation for clarity

--- src/main/kotlin/entity/BonsaiGame.kt
+++ src/main/kotlin/entity/BonsaiGame.kt
@@ -5,13 +5,14 @@ import tools.aqua.bgw.util.Stack
 /**
  * Entity to represent a game of "Bonsai"
  *
- * @property undoStack Saves previous game state.
- * @property redoStack Saves game states reverted by undo.
  * @param gameSpeed Value for the simulation speed
  * @param drawStack The card stack in the shop
  * @param openCards List of the available cards in the shop
  * @param players List of players in the configured sorting order
  * @param goalCards List of chosen goal cards
+ * @property undoStack Saves previous game state.
+ * @property redoStack Saves game states reverted by undo.
+ * @property currentState The current game state.
  */
 class BonsaiGame(
     gameSpeed: Int,
