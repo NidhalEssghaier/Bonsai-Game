774f8c2c8236c4badf5e041a1dcf4246b269c6d4
Silas Menge <silas.menge@tu-dortmund.de>
2025-03-15 19:36:17 +0100

    Fix tests

--- src/test/kotlin/service/EndGameTest.kt
+++ src/test/kotlin/service/EndGameTest.kt
@@ -8,58 +8,53 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class EndGameTest {
-
     @Test
     fun testEndGame() {
-        //start a game
+        // start a game
         val rootService = RootService()
         val testRefreshable = TestRefreshable()
         rootService.addRefreshable(testRefreshable)
         val gameService = rootService.gameService
         assertFalse { testRefreshable.refreshAfterEndGameCalled }
 
-        val player0 = Triple("Alice",0, PotColor.RED)
-        val player1 = Triple("Bob",0, PotColor.PURPLE)
-        val players = mutableListOf(player0,player1)
+        val player0 = Triple("Alice", 0, PotColor.RED)
+        val player1 = Triple("Bob", 0, PotColor.PURPLE)
+        val players = mutableListOf(player0, player1)
 
-        val goalCards = listOf(
-            GoalCard(5, GoalColor.BROWN, GoalDifficulty.LOW),
-            GoalCard(10, GoalColor.BROWN, GoalDifficulty.INTERMEDIATE),
-            GoalCard(15, GoalColor.BROWN, GoalDifficulty.HARD),
+        val goalColors = listOf(GoalColor.BROWN, GoalColor.GREEN, GoalColor.ORANGE)
 
-            GoalCard(6, GoalColor.GREEN, GoalDifficulty.LOW),
-            GoalCard(9, GoalColor.GREEN, GoalDifficulty.INTERMEDIATE),
-            GoalCard(12, GoalColor.GREEN, GoalDifficulty.HARD),
-
-            GoalCard(9, GoalColor.ORANGE, GoalDifficulty.LOW),
-            GoalCard(11, GoalColor.ORANGE, GoalDifficulty.INTERMEDIATE),
-            GoalCard(13, GoalColor.ORANGE, GoalDifficulty.HARD)
-        )
-
-        gameService.startNewGame(players,3,goalCards)
+        gameService.startNewGame(players, 3, goalColors)
 
         val game = rootService.currentGame
         checkNotNull(game)
 
-        game.currentState.players[0].bonsai.grid[0, -1] = BonsaiTile(TileType.WOOD) //0
-        game.currentState.players[0].bonsai.grid[1, -1] = BonsaiTile(TileType.LEAF) //3
-        game.currentState.players[0].bonsai.grid[1, -2] = BonsaiTile(TileType.LEAF) //3
-        game.currentState.players[0].bonsai.grid[1, -3] = BonsaiTile(TileType.FLOWER) //5
-        game.currentState.players[0].bonsai.grid[2, -2] = BonsaiTile(TileType.FRUIT) //7
+        game.currentState.players[0]
+            .bonsai.grid[0, -1] = BonsaiTile(TileType.WOOD) // 0
+        game.currentState.players[0]
+            .bonsai.grid[1, -1] = BonsaiTile(TileType.LEAF) // 3
+        game.currentState.players[0]
+            .bonsai.grid[1, -2] = BonsaiTile(TileType.LEAF) // 3
+        game.currentState.players[0]
+            .bonsai.grid[1, -3] = BonsaiTile(TileType.FLOWER) // 5
+        game.currentState.players[0]
+            .bonsai.grid[2, -2] = BonsaiTile(TileType.FRUIT) // 7
 
-        game.currentState.players[0].acceptedGoals.add(goalCards[0]) //5
+        game.currentState.players[0]
+            .acceptedGoals
+            .add(game.currentState.goalCards[0]) // 5
 
-        val parchmentCard = ParchmentCard(1,ParchmentCardType.WOOD,99)
-        game.currentState.players[0].hiddenDeck.add(parchmentCard)
-        //+2 through wood tiles and parchment card
+        val parchmentCard = ParchmentCard(1, ParchmentCardType.WOOD, 99)
+        game.currentState.players[0]
+            .hiddenDeck
+            .add(parchmentCard)
+        // +2 through wood tiles and parchment card
 
         assertThrows<IllegalStateException> { gameService.endGame() }
         game.currentState.drawStack.clear()
 
         val points = gameService.endGame()
         val player0Points = points[0].second
-        assertEquals(player0Points,25)
+        assertEquals(player0Points, 25)
         assertTrue { testRefreshable.refreshAfterEndGameCalled }
-
     }
 }
--- src/test/kotlin/service/StartNewGameTest.kt
+++ src/test/kotlin/service/StartNewGameTest.kt
@@ -1,8 +1,6 @@
 package service
 
-import entity.GoalCard
 import entity.GoalColor
-import entity.GoalDifficulty
 import entity.PotColor
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -19,53 +17,40 @@ class StartNewGameTest {
         val gameService = rootService.gameService
         assertFalse { testRefreshable.refreshAfterStartNewGameCalled }
 
-        //create players (two local players)
-        val player0 = Triple("Alice",0,PotColor.RED)
-        val player1 = Triple("Bob",0,PotColor.PURPLE)
-        val players = mutableListOf(player0,player1)
+        // create players (two local players)
+        val player0 = Triple("Alice", 0, PotColor.RED)
+        val player1 = Triple("Bob", 0, PotColor.PURPLE)
+        val players = mutableListOf(player0, player1)
 
-        val goalCards = listOf(
-            GoalCard(5,GoalColor.BROWN,GoalDifficulty.LOW),
-            GoalCard(10,GoalColor.BROWN,GoalDifficulty.INTERMEDIATE),
-            GoalCard(15,GoalColor.BROWN,GoalDifficulty.HARD),
+        val goalColors = listOf(GoalColor.BROWN, GoalColor.GREEN, GoalColor.ORANGE)
 
-            GoalCard(6,GoalColor.GREEN,GoalDifficulty.LOW),
-            GoalCard(9,GoalColor.GREEN,GoalDifficulty.INTERMEDIATE),
-            GoalCard(12,GoalColor.GREEN,GoalDifficulty.HARD),
-
-            GoalCard(9,GoalColor.ORANGE,GoalDifficulty.LOW),
-            GoalCard(11,GoalColor.ORANGE,GoalDifficulty.INTERMEDIATE),
-            GoalCard(13,GoalColor.ORANGE,GoalDifficulty.HARD)
-        )
-
-        gameService.startNewGame(players,3,goalCards)
+        gameService.startNewGame(players, 3, goalColors)
         assertTrue { testRefreshable.refreshAfterStartNewGameCalled }
 
         var game = rootService.currentGame
         checkNotNull(game)
         assertEquals(game.currentState.drawStack.size, 28)
 
-        //test wrong initialization
-        val playerWrong = Triple("Error",5,PotColor.BLUE)
+        // test wrong initialization
+        val playerWrong = Triple("Error", 5, PotColor.BLUE)
         players.add(playerWrong)
-        assertThrows<IllegalArgumentException> { gameService.startNewGame(players,2,goalCards) }
+        assertThrows<IllegalArgumentException> { gameService.startNewGame(players, 2, goalColors) }
         players.remove(playerWrong)
 
-        //test for three players (two players and a bot)
-        val bot1 = Triple("Random",2,PotColor.BLUE)
+        // test for three players (two players and a bot)
+        val bot1 = Triple("Random", 2, PotColor.BLUE)
         players.add(bot1)
-        gameService.startNewGame(players,1,goalCards)
+        gameService.startNewGame(players, 1, goalColors)
         game = rootService.currentGame
         checkNotNull(game)
         assertEquals(game.currentState.drawStack.size, 39)
 
-        //test for four players (two players and two bots)
-        val bot2 = Triple("Smart",3,PotColor.GRAY)
+        // test for four players (two players and two bots)
+        val bot2 = Triple("Smart", 3, PotColor.GRAY)
         players.add(bot2)
-        gameService.startNewGame(players,2,goalCards)
+        gameService.startNewGame(players, 2, goalColors)
         game = rootService.currentGame
         checkNotNull(game)
         assertEquals(game.currentState.drawStack.size, 43)
-
     }
 }
