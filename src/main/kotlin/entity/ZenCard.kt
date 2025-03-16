443d0167a414251218d41300fb76e8006f421f48
Ao Zhong <hacc1225@gmail.com>
2025-03-16 20:48:08 +0100

    feat: Implement game save and load functionality

--- src/main/kotlin/entity/ZenCard.kt
+++ src/main/kotlin/entity/ZenCard.kt
@@ -1,9 +1,11 @@
 package entity
 
+import kotlinx.serialization.Polymorphic
 import kotlinx.serialization.Serializable
 
 /**
  * Interface to represent a zen card
  */
 @Serializable
+@Polymorphic
 sealed interface ZenCard
